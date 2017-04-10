ee4bc0c41f959c6e6b492da2c5fce415e158f131
Martin R. Tolava <martintolava@gmail.com>
2017-04-10 18:09:45 -0300

    Creando plugin qosqo bonocreditos para la cátedra de proyectos de ISI de la UTN-FRT

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :qosqo_bonocreditos do
+  name 'Qosqo Bonocreditos plugin'
+  author 'Martín Roberto Tolava'
+  description 'Visualizar la cantidad de bonocréditos de alumnos. Para aprobar la materia Proyecto de Ing. Sistemas de Información de UTN-FRT'
+  version '0.0.1'
+  url 'http://github.com'
+  author_url 'http://project.i4dev.com.ar'
+end
