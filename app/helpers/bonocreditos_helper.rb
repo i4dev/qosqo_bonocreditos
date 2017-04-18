0538b6b68b2f0ace6902a397dfa5c3f63d5ac637
Eduardo S Terrones <terrones.es@gmail.com>
2017-04-18 13:22:17 -0300

    Coloreando la tabla de bonocreditos acorde al estado

--- app/helpers/bonocreditos_helper.rb
+++ app/helpers/bonocreditos_helper.rb
@@ -1,2 +1,17 @@
 module BonocreditosHelper
+  def clase_bonocredito(valor)
+    if valor>5
+      return "green"
+    else
+      return "red"
+    end
+  end
+
+  def clase_bonocredito_alumno(alumno)
+    if alumno[:bonos_cyc]>5 && alumno[:bonos_rs]>5 && alumno[:bonos_sdi]>5
+      return "green"
+    else
+      return ""
+    end
+  end
 end
