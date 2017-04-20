cc0916e3b0bd4532771247b48f272e4d3526b0e8
Eduardo S Terrones <terrones.es@gmail.com>
2017-04-20 20:14:45 -0300

    Omitir alumnos de subproyectos no activos

--- app/controllers/bonocreditos_controller.rb
+++ app/controllers/bonocreditos_controller.rb
@@ -11,7 +11,7 @@ class BonocreditosController < ApplicationController
       if @rol.nil? #si => campo está vacío
         @rol=Role.find_by_name('Scrum Team') #entonces => rol = Scrum Team
       end
-      @project.members.each do |member| # Por cada miembro del proyecto hacer 
+      @project.members.each do |member| # Por cada miembro del proyecto hacer
         if member.roles.include?(@rol) #Si => el rol del miembro es 'Scrum Team'
           bonos_cyc=@project.issues.where(assigned_to_id: member.user_id, tipo_bonocredito:"Conferencias y Congresos", tracker_id: @project.parent.bonocredito_tracker_id).sum(:bonocreditos).to_i
           bonos_rs=@project.issues.where(assigned_to_id: member.user_id, tipo_bonocredito:"Responsabilidad Social", tracker_id: @project.parent.bonocredito_tracker_id).sum(:bonocreditos).to_i
@@ -26,13 +26,15 @@ class BonocreditosController < ApplicationController
         @rol=Role.find_by_name('Scrum Team')
       end
       @project.children.each do |subproject|
-        subproject.members.each do |member|
-          if member.roles.include?(@rol)
-            bonos_cyc=subproject.issues.where(assigned_to_id: member.user_id, tipo_bonocredito:"Conferencias y Congresos", tracker_id: @project.bonocredito_tracker_id).sum(:bonocreditos).to_i
-            bonos_rs=subproject.issues.where(assigned_to_id: member.user_id, tipo_bonocredito:"Responsabilidad Social", tracker_id: @project.bonocredito_tracker_id).sum(:bonocreditos).to_i
-            bonos_sdi=subproject.issues.where(assigned_to_id: member.user_id, tipo_bonocredito:"Semana de la Ingeniería", tracker_id: @project.bonocredito_tracker_id).sum(:bonocreditos).to_i
-            total=bonos_rs+bonos_cyc+bonos_sdi
-            @alumnos<<{user: member.user, bonos_cyc: bonos_cyc, bonos_rs: bonos_rs, bonos_sdi: bonos_sdi, total: total}
+        unless !subproject.active? #Solo entra a analizar los miembros en proyectos activos, no cerrados!!
+          subproject.members.each do |member|
+            if member.roles.include?(@rol)
+              bonos_cyc=subproject.issues.where(assigned_to_id: member.user_id, tipo_bonocredito:"Conferencias y Congresos", tracker_id: @project.bonocredito_tracker_id).sum(:bonocreditos).to_i
+              bonos_rs=subproject.issues.where(assigned_to_id: member.user_id, tipo_bonocredito:"Responsabilidad Social", tracker_id: @project.bonocredito_tracker_id).sum(:bonocreditos).to_i
+              bonos_sdi=subproject.issues.where(assigned_to_id: member.user_id, tipo_bonocredito:"Semana de la Ingeniería", tracker_id: @project.bonocredito_tracker_id).sum(:bonocreditos).to_i
+              total=bonos_rs+bonos_cyc+bonos_sdi
+              @alumnos<<{user: member.user, bonos_cyc: bonos_cyc, bonos_rs: bonos_rs, bonos_sdi: bonos_sdi, total: total}
+            end
           end
         end
       end
